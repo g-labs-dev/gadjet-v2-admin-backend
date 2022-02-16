53ffe0af353632cc44aa885b191e75adfc214370
Kang Byung Seon <rkdqudtjs1@naver.com>
2022-02-16 14:22:12 +0900

    chore: init

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const express_1 = (0, tslib_1.__importDefault)(require("express"));
+const app = (0, express_1.default)();
+app.listen(() => {
+    console.log('app listen');
+});
+//# sourceMappingURL=index.js.map
