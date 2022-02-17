7152488c07767bd5dc066a3e38ef5912382d1531
Kang Byung Seon <rkdqudtjs1@naver.com>
2022-02-17 13:30:32 +0900

    chore: apply .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = (0, tslib_1.__importDefault)(require("express"));
-const app = (0, express_1.default)();
-app.listen(() => {
-    console.log('app listen');
-});
-//# sourceMappingURL=index.js.map
