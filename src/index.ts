e19e0df023e60667f2d777b2957f61c3a52ce589
Kang Byung Seon <rkdqudtjs1@naver.com>
2022-02-17 13:29:43 +0900

    chore: deploy 설정

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
 import routes from '@routes';
+import cors from 'cors';
+import express from 'express';
 
 import { initModels } from '@utils/sequelize';
 
-const port = 8085 as const;
+const port = process.env.PORT || 8085;
 const host = '0.0.0.0' as const;
 
 const app = express();
@@ -16,7 +16,7 @@ app.use('/api', routes());
 
 initModels();
 
-const listener = app.listen(port, host, () => {
+const listener = app.listen(Number(port), host, () => {
   const { port, address } = listener.address() as { port: number; address: string };
-  console.log(`App is listening : ${address}:${port}`);
+  console.log(`App is listening [${process.env.NODE_ENV}]: ${address}:${port}`);
 });
