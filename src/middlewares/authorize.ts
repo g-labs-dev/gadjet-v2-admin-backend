dfd2ec70a3a8ec5ac50e788161040b58321d6cd8
이태호 <rami9014@gmail.com>
2022-02-22 16:49:43 +0900

    feat : 토큰 header.authorization 에 추가하도록 수정

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import * as authService from '@services/auth.service';
 
 export const authorizeMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.token;
+  const token = req.headers.authorization;
 
   const isValid = authService.validateToken(String(token));
 
